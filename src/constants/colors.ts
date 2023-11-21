e911680763072a1b01efb8c1cc2868794b05cff3
Farhan Tariq <farhantariq@gmail.com>
2023-11-21 11:51:25 +0500

    adds constants and interfaces

---
+++ src/constants/colors.ts
@@ -0,0 +1,8 @@
+export const Colors = {
+  Black: "#000",
+  White: "#FFF",
+  Purple: "#BF5AF2",
+  Yellow: "#FFD60A",
+  Blue: "#0A84FF",
+  Cyan: "#64D2FF",
+};
---
+++ src/constants/routes.ts
@@ -0,0 +1,4 @@
+export const routes = {
+  home: "/",
+  fiveDay: "/5days",
+};
